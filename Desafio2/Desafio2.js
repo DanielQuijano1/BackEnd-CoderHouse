fd56c31c71b2f218e3bc2243954b0f0911c972d9
Daniel <hector.daniel.quijano@gmail.com>
2023-08-11 18:57:31 -0300

    arreglos aaa

--- Desafio2/Desafio2.js
+++ Desafio2/Desafio2.js
@@ -1,11 +1,9 @@
-import {promises as fs} from 'fs'
-
-const path = "./productos.json"
+import { promises as fs } from 'fs'
 
 class ProductManager {
 
     constructor() {
-        this.path = path;
+        this.path = "./Desafio2/productos.json";
         this.products = [];
     }
 
@@ -27,12 +25,8 @@ class ProductManager {
 
 
     async getProducts() {
-        try {
-            this.products = JSON.parse(await fs.readFile(this.path, "utf-8"))
-            console.log(this.products)
-        } catch (error) {
-            console.log(error)
-        }
+            const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
+            console.log(products)
     }
 
     getProductsByID = async (id) => {
@@ -47,24 +41,29 @@ class ProductManager {
     }
 
     updateProduct = async (id, data) => {
-        try {
-            let productUpdate = await this.getProductsByID(id);
-            let productIndex = this.products.findIndex(e => e.id === id)
-            this.products[productIndex] = { ...productUpdate, ...data, id: id }
-            await fs.writeFile(this.path, JSON.stringify(this.products))
-            console.log("producto editado correctamente")
-        } catch(error){
-            console.log("Error en el UPDATE", error)
+        const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        const indiceProducto = products.findIndex(e => e.id === id)
+        if (indiceProducto != -1) {
+            products[indiceProducto].code = data.code
+            products[indiceProducto].title = data.title
+            products[indiceProducto].price = data.price
+            products[indiceProducto].thumbnail = data.thumbnail
+            products[indiceProducto].stock = data.stock
+            products[indiceProducto].category = data.category
+            products[indiceProducto].description = data.description
+            await fs.writeFile(this.path, JSON.stringify(products))
+        } else {
+            console.log("Producto no encontrao")
         }
     }
 
-    deleteProduct = async(id) => {
-        try{
-            this.products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
-            this.products = this.products.filter(product => product.id !== id)
-            await fs.writeFile(this.path, JSON.stringify(this.products))
-        }catch (error){
-            console.log("Error en Borrar el producto", error)
+    deleteProduct = async (id) => {
+        const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        const producto = products.find(e => e.id === id)
+        if (producto) {
+            await fs.writeFile(this.path, JSON.stringify(products.filter(e => e.id != id)))
+        } else {
+            console.log("Error en Borrar el producto")
         }
     }
 }
@@ -96,7 +95,7 @@ class Product {
     }
 
 }
-
+/*
 
 //Testing
 
@@ -104,10 +103,11 @@ class Product {
 const producto1 = new Product('FF001', "Funda mototola g8", 250, [], 500, "fundas", "funda para motorola g8")
 const producto2 = new Product('FF002', "Funda mototola g8 power", 250, [], 500, "fundas", "funda para motorola g8 power")
 const producto3 = new Product('FF003', "Funda mototola g8 plus", 250, [], 500, "fundas", "funda para motorola g8 plus")
-
+*/
 // se crea la instancia Product Manager
-const adminProduct = new ProductManager("./productos.json");
+const adminProduct = new ProductManager("./Desafio2/productos.json");
 
+/*
 //se llama a getProducts la cual debe devolver un arreglo vacio
 console.log(adminProduct.getProducts());
 
@@ -115,6 +115,7 @@ console.log(adminProduct.getProducts());
 adminProduct.addProduct(producto1)
 adminProduct.addProduct(producto2)
 adminProduct.addProduct(producto3)
+
 
 //se llama a getProducts de nuevo, esta vez tiene que aparecer el producto ya creado
 console.log(adminProduct.getProducts());
@@ -125,4 +126,25 @@ adminProduct.addProduct(producto2)
 //se evalua getProductsByID para devuelva error si no encuentra el producto, de caso contrario se devuelve el producto
 console.log(adminProduct.getProductsByID(15));
 
-console.log(adminProduct.getProductsByID(1));
+
+//se llama a updateProduct para actualizar la data de uno de los productos
+const productoActualizado =
+{
+    code: "FF003",
+    title: "Funda mototola g8 plus lite",
+    price: 250,
+    thumbnail: [],
+    stock: 50,
+    category: "fundas",
+    description: "funda para motorola g8 plus lite",
+}
+
+adminProduct.updateProduct(2, productoActualizado )
+*/
+// adminProduct.deleteProduct(2)
+
+//se evalua update del producto 2 con getProductsByID
+console.log(adminProduct.getProducts());
+// console.log(adminProduct.getProductsByID(2));
+
+
